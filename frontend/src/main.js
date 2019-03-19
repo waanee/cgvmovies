89f6aca01acc0c087b487577c605865d49f7192b
waanee kim (호이호이) <ksw654@gmail.com>
2019-03-19 18:12:36 +0900

    uikit add

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import axios from 'axios'
+import UIkit from 'uikit'
+import Icons from 'uikit/dist/js/uikit-icons'
 
 Vue.config.productionTip = false
 Vue.prototype.$http = axios;
