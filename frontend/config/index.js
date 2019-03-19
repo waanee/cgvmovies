cd13eb11b8516722077140b14ee9ad84801eba85
waanee kim (호이호이) <ksw654@gmail.com>
2019-03-19 11:00:52 +0900

    config setting

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -10,7 +10,15 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/api':{
+        target: 'http://localhost:3000/api',
+        changeOrigin: true,
+        pathRewrite:{
+          '^/api':''
+        }
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
@@ -45,10 +53,10 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../../backend/public/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../../backend/public'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
 
