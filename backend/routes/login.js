de9860cdcfe51bf5d6b79c1f8da9f507688c4128
waanee kim (호이호이) <ksw654@gmail.com>
2019-04-04 22:36:09 +0900

    cgvapp

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,11 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+const logger = require('morgan')
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-    
-    
-    res.send('respond with a resource');
-});
+var mysql = require('mysql');
+var mysqlConfig = require('../dbconfig.js');
+var connection = mysql.createConnection(mysqlConfig);
+
+router.get('/', function(req,res) {
+	var msg;
+	var errMsg = req.flash('error')
+	if(errMsg) msg = errMsg;
+	res.send('sd'+msg);
+})
 
 module.exports = router;
